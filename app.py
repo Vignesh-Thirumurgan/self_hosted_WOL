3db16d83be36765c96a5243a20b989222127bc73
Craze477 <mvignesh477@gmail.com>
2024-10-08 17:55:03 +0530

    push16

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__, template_folder='templates', static_url_path='/')
 app.secret_key = 'your_secret_key'  # Set your secret key for session management
 
 # MySQL configurations  
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'db'
 app.config['MYSQL_USER'] = 'WAKE'
 app.config['MYSQL_PASSWORD'] = 'WAKE'
 app.config['MYSQL_DB'] = 'WAKE'
